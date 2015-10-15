afcc0fe0109caf5ed8819fb8251dcba0a37e044e
toomaj <toomaj_b@yahoo.com>
2015-10-15 11:39:11 +1000

    add entity_controller spec

--- spec/controllers/api/entity_controller_spec.rb
+++ spec/controllers/api/entity_controller_spec.rb
@@ -2,5 +2,28 @@ require 'rails_helper'
 
 module API
   RSpec.describe EntityController, type: :controller do
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index, format: :json
+        expect(response).to have_http_status(:success)
+      end
+
+      let(:json) { JSON.parse(response.body) }
+
+      context 'get /entities' do
+        def run
+          get :index, format: :json
+        end
+
+        let!(:entity) { create(:known_entity) }
+        before { run }
+
+        it 'gets the entities' do
+          # not_to include for instance
+          expect(json[:entity])
+            .not_to include(not_to: 'not_to')
+        end
+      end
+    end
   end
 end
