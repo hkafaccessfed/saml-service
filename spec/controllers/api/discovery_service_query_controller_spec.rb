2aff129ffbca1dfb7fc53fac626558d4b8339d11
toomaj <toomaj_b@yahoo.com>
2015-10-21 14:16:36 +1000

    Remove unused let(){} block

--- spec/controllers/api/discovery_service_query_controller_spec.rb
+++ spec/controllers/api/discovery_service_query_controller_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe API::DiscoveryServiceQueryController, type: :controller do
   describe 'GET #index' do
-    let(:primary_tag) { Faker::Lorem.word }
-
     it 'returns http success' do
       get :index, format: :json
       expect(response).to have_http_status(:success)
